57b426782aa1d1720b07aa3ebff0fbefc79fc393
jsc9475 <jsc9475@naver.com>
2021-08-04 10:26:20 +0900

    로아 경매장 수수료 수정본
    ㅁ

--- CPPstudy.cpp
+++ CPPstudy.cpp
@@ -39,7 +39,6 @@ int seller()
 		}
 	}
 	std::cout << "수수료를 제외한 합계는 " << sum << "원입니다" << std::endl;
-	Sleep(20000);
 	return 0;
 }
 int buyer()
@@ -47,7 +46,7 @@ int buyer()
 	int i = 0, num = 0, sum = 0, gagong=0;
 	int numArray[100]{};
 	int num1Array[100]{};
-	int wanNumber = 0, wanPrice = 0, wanSu = 0, wanze=0;
+	int wanNumber = 0, wanPrice = 0, wanze=0;
 	std::cout << "재료는 총 몇종류? (ex> 400원*10개면 공통인자이므로 1종류로 칩니다. ): ";
 	std::cin >> num;
 	for (i = 0; i < num; i++)
@@ -60,6 +59,7 @@ int buyer()
 		if (numArray[i] <= 0)
 		{
 			std::cout << "마이너스 값은 할당하지 않습니다 즉시 종료합니다. 지금까지 계산한 값의 수수료합은 " << sum << "입니다." << std::endl;
+			Sleep(4000);
 		}
 	}
 	std::cout << "재료의 총 가격은 " << sum << "원입니다" << std::endl;
@@ -74,22 +74,19 @@ int buyer()
 		std::cout << "수수료는 " << 1 * wanNumber << "원" << std::endl;
 		wanze += (wanPrice * wanNumber-1*wanNumber);
 	}
-	else if (wanSu > 20)
+	else if (wanPrice > 20)
 	{
 		int charge = (wanPrice / 20);
 		std::cout << "수수료는 " << charge * wanNumber << "원" << std::endl;
-		wanze += ((wanPrice - charge) * wanNumber);
+		wanze += ((wanPrice * wanNumber) - (charge * wanNumber));
 	}
 	std::cout << "총 판매금액은 " << wanze << "원이고 차익은 " << wanze - (sum + gagong) << "원입니다"<<std::endl;
 	return 0;
-};
+}
 
 int main(void)
 {
-	int i = 0, num = 0, sum = 0;
-	int numArray[100]{};
-	int num1Array[100]{};
-	//sum값은 수수료를 제한 금액들의 합
+	int i;
 	while (1) {
 		std::cout << "1.판매자입장   2.구매자입장    3.종료    1,2,3중 선택 : ";
 		std::cin >> i;
