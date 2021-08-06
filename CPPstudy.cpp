3dee2f56ea3623a6e6c473051f34a75aae4b0d8f
jsc9475 <jsc9475@naver.com>
2021-08-06 23:15:55 +0900

    c++어려워
    c++어려워

--- CPPstudy.cpp
+++ CPPstudy.cpp
@@ -1,105 +1,21 @@
-#include<iostream>
-#include<stdlib.h>
-#include<string.h>
-#include <cmath>
-#include<windows.h>
+#include <iostream>
+#include <string>
+#include <cstdio>
 
-int seller()
+int main(int argc, char* argv[])
 {
-	int i = 0, num = 0, sum = 0;
-	int numArray[100]{};
-	int num1Array[100]{};
-	std::cout << "등록할 물건은 몇개? (ex> 400원*10개면 공통인자이므로 1개로 칩니다. ): ";
-	std::cin >> num;
-	for (i = 0; i < num; i++)
-	{
-		std::cout << "물품의 가격을 입력해주세요 : ";
-		std::cin >> numArray[i];
-		std::cout << "물품의 개수를 입력해주세요 : ";
-		std::cin >> num1Array[i];
-		if (numArray[i] == 1)
-		{
-			std::cout << "수수료는 " << 1 * num1Array[i] << "원" << std::endl;
-			sum += (numArray[i] * num1Array[i]);
-		}
-		else if (numArray[i] <= 20 && numArray[i] > 1)
-		{
-			std::cout << "수수료는 " << 1 * num1Array[i] << "원" << std::endl;
-			sum += ((numArray[i] - 1) * num1Array[i]);
-		}
-		else if (numArray[i] > 20)
-		{
-			int charge = (numArray[i] / 20);
-			std::cout << "수수료는 " << charge * num1Array[i] << "원" << std::endl;
-			sum += ((numArray[i] - charge) * num1Array[i]);
-		}
-		else if (numArray[i] <= 0)
-		{
-			std::cout << "마이너스 값은 할당하지 않습니다 즉시 종료합니다. 지금까지 계산한 값의 수수료합은 " << sum << "입니다." << std::endl;
-		}
-	}
-	std::cout << "수수료를 제외한 합계는 " << sum << "원입니다" << std::endl;
-	return 0;
-}
-int buyer()
-{
-	int i = 0, num = 0, sum = 0, gagong=0;
-	int numArray[100]{};
-	int num1Array[100]{};
-	int wanNumber = 0, wanPrice = 0, wanze=0;
-	std::cout << "재료는 총 몇종류? (ex> 400원*10개면 공통인자이므로 1종류로 칩니다. ): ";
-	std::cin >> num;
-	for (i = 0; i < num; i++)
-	{
-		std::cout << "물품의 가격을 입력해주세요 : ";
-		std::cin >> numArray[i];
-		std::cout << "물품의 개수를 입력해주세요 : ";
-		std::cin >> num1Array[i];
-		sum += (numArray[i] * num1Array[i]);
-		if (numArray[i] <= 0)
-		{
-			std::cout << "마이너스 값은 할당하지 않습니다 즉시 종료합니다. 지금까지 계산한 값의 수수료합은 " << sum << "입니다." << std::endl;
-			Sleep(4000);
-		}
-	}
-	std::cout << "재료의 총 가격은 " << sum << "원입니다" << std::endl;
-	std::cout << "가공 비용을 입력하세요 : ";
-	std::cin >> gagong;
-	std::cout << "완제품 개수를 입력하세요 : ";
-	std::cin >> wanNumber;
-	std::cout << "완제품 개당 가격을 입력하세요 : ";
-	std::cin >> wanPrice;
-	if (wanPrice <= 20 && wanPrice >= 1)
-	{
-		std::cout << "수수료는 " << 1 * wanNumber << "원" << std::endl;
-		wanze += (wanPrice * wanNumber-1*wanNumber);
-	}
-	else if (wanPrice > 20)
-	{
-		int charge = (wanPrice / 20);
-		std::cout << "수수료는 " << charge * wanNumber << "원" << std::endl;
-		wanze += ((wanPrice * wanNumber) - (charge * wanNumber));
-	}
-	std::cout << "총 판매금액은 " << wanze << "원이고 차익은 " << wanze - (sum + gagong) << "원입니다"<<std::endl;
-	return 0;
-}
+	int nAge;
+	std::cout << "나이" << std::endl;
+	std::cin >> nAge;
 
-int main(void)
-{
-	int i;
-	while (1) {
-		std::cout << "1.판매자입장   2.구매자입장    3.종료    1,2,3중 선택 : ";
-		std::cin >> i;
-		if (i == 1)
-		{
-			seller();
-		}
-		else if (i == 2)
-		{
-			buyer();
-		}
-		else if (i == 3)
-			return 0;
-	}
+	char A[32];
+	std::cout << "직업" << std::endl;
+	std::cin >> A;
+
+	std::string strName;
+	std::cout << "이름" << std::endl;
+	std::cin >> strName;
+
+	std::cout << "당신의 이름은 " << strName << "이고, " << "나이는 " << nAge << "이며, " << "직업은 " << A << "입니다."<<std::endl;
 	return 0;
 }
