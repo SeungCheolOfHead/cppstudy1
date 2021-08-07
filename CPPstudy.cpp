a603019e0eba3031fdf1d448c701f8b6d368011a
jsc9475 <jsc9475@naver.com>
2021-08-07 21:43:00 +0900

    c++

--- CPPstudy.cpp
+++ CPPstudy.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 #include <string>
 #include <cstdio>
+using namespace std;
+
+void TestFunc(int& rparam)
+{
+	rparam = 100;
+}
 
 int main(int argc, char* argv[])
 {
-	int nAge;
-	std::cout << "나이" << std::endl;
-	std::cin >> nAge;
-
-	char A[32];
-	std::cout << "직업" << std::endl;
-	std::cin >> A;
-
-	std::string strName;
-	std::cout << "이름" << std::endl;
-	std::cin >> strName;
-
-	std::cout << "당신의 이름은 " << strName << "이고, " << "나이는 " << nAge << "이며, " << "직업은 " << A << "입니다."<<std::endl;
+	int nData = 0;
+	TestFunc(nData);
+	cout << nData << endl;
 	return 0;
 }
