7b9726df6bfe53d338e924d3fb4577ace37516bb
jsc9475 <jsc9475@naver.com>
2021-08-14 12:56:27 +0900

    c++

--- CPPstudy.cpp
+++ CPPstudy.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
 #include <algorithm>
+#include<windows.h>
 using namespace std;
+
+int SangUn2(int sum)
+{
+	for (int i = 0; i < 2; i++) {
+		cout << "상언 조합중";
+		cout << ".";
+		Sleep(500);
+		cout << ".";
+		Sleep(500);
+		cout << ".";
+		Sleep(500);
+		cout << ".";
+		Sleep(500);
+		cout << ".";
+		Sleep(500);
+		cout << ".";
+		Sleep(500);
+		cout << endl;
+	}
+	cout << "상언의 연봉은 "<<sum<<"원 입니다" <<endl;
+	return 0;
+}
 
 int main(int argc, char* argv[])
 {
-	
+	int SangUn[12] = {};
+	int sum = 0;
+	for (int i = 0; i < 12; i++)
+	{
+		cout << "상언 "<<i+1<<"웓 월급 : ";
+		cin >> SangUn[i];
+		sum += SangUn[i];
+	}
+	SangUn2(sum);
+	return 0;
 }
