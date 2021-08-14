9292d251464c58f0b50ad61cbc28f2302fff79f2
jsc9475 <jsc9475@naver.com>
2021-08-14 12:33:57 +0900

    c

--- CPPstudy.cpp
+++ CPPstudy.cpp
@@ -2,14 +2,7 @@
 #include <algorithm>
 using namespace std;
 
-template<typename T>
-T Add(T a, T b)
-{
-	return a + b;
-}
 int main(int argc, char* argv[])
 {
-	cout << Add<int>(3, 4) << endl;
-	cout << Add(3.3, 4.4) << endl;
-	return 0;
+	
 }
