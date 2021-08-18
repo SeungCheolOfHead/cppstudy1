0af501c6462bd8a4250970b382adad0909e1456f
jsc9475 <jsc9475@naver.com>
2021-08-18 23:13:57 +0900

    c++

--- CPPstudy.cpp
+++ CPPstudy.cpp
@@ -1,4 +1,19 @@
-#include<iostream>
+#include <iostream>
+#include "TCHAR.h"
 using namespace std;
 
-a
+int nData(20);
+
+namespace TEST
+{
+	void TestFunc(void) {
+		cout << nData << endl;
+	}
+	int nData = 100;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	TEST::TestFunc();
+	return 0;
+}
