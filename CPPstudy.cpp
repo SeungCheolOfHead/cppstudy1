d30ad974a398cf284b85e61b4edf711904113fc2
jsc9475 <jsc9475@naver.com>
2021-08-23 01:20:40 +0900

    c++

--- CPPstudy.cpp
+++ CPPstudy.cpp
@@ -2,26 +2,41 @@
 #include "TCHAR.h"
 using namespace std;
 
-class CTest
+class CMyPoint
 {
-	int nData;
 public:
-	CTest() 
+	CMyPoint(int x)
 	{
-		cout << "CTest::CTest()" << endl;
+		cout << "CMyPoint(int)" << endl;
+		if (x > 100)
+		{
+			x = 100;
+		}
+		m_x = 100;
 	}
-	~CTest() 
+	CMyPoint(int x, int y)
+		:CMyPoint(x)
 	{
-		cout << "~CTest::CTest() " << endl;
+		cout << "CMyPoint(int,int)" << endl;
+		if (y > 200)
+			y = 200;
+		m_y = 200;
 	}
+	void Print()
+	{
+		cout << "X:" << m_x << endl;
+		cout << "Y:" << m_y << endl;
+	}
+private:
+	int m_x = 0;
+	int m_y = 0;
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	cout << "Begin" << endl;
-	CTest* p = new CTest[5050];
-	cout << "Test" << endl;
-	delete[] p;
-	cout << "End" << endl;
+	CMyPoint ptBegin(110);
+	ptBegin.Print();
+	CMyPoint ptEnd(50, 250);
+	ptEnd.Print();
 	return 0;
 }
