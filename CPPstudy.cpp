656fa564a79f9562e93c17eaf1ac9dbf67fa5a77
jsc9475 <jsc9475@naver.com>
2021-08-23 23:59:56 +0900

    c++
    c++

--- CPPstudy.cpp
+++ CPPstudy.cpp
@@ -5,38 +5,17 @@ using namespace std;
 class CMyPoint
 {
 public:
-	CMyPoint(int x)
+	CMyPoint(int n) : nData(n) {}
+	void PrintData()
 	{
-		cout << "CMyPoint(int)" << endl;
-		if (x > 100)
-		{
-			x = 100;
-		}
-		m_x = 100;
+		cout << nData << endl;
+		cout << this->nData << endl;
 	}
-	CMyPoint(int x, int y)
-		:CMyPoint(x)
-	{
-		cout << "CMyPoint(int,int)" << endl;
-		if (y > 200)
-			y = 200;
-		m_y = 200;
-	}
-	void Print()
-	{
-		cout << "X:" << m_x << endl;
-		cout << "Y:" << m_y << endl;
-	}
-private:
-	int m_x = 0;
-	int m_y = 0;
 };
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	CMyPoint ptBegin(110);
-	ptBegin.Print();
-	CMyPoint ptEnd(50, 250);
-	ptEnd.Print();
+	CMyPoint a(5);
+	a.PrintData();
 	return 0;
 }
